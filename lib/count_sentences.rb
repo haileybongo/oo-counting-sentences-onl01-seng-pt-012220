9f4a163c5e6a7b6a3c08d81d5eb7516ed673d8a3
Hailey Bongo <haileyebongo@gmail.com>
2020-02-09 22:48:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,7 +21,7 @@ class String
     string_array = self.split(/[.?!]/)
     string_array.map do |sentence|
       if sentence.size == 0 
-        sentence.remove 
+        sentence.delete  
       end
     end
     string_array.count

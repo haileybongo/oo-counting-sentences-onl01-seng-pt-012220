6bbbe2b1568ffa3203af2e96181078bbd55f3a90
Hailey Bongo <haileyebongo@gmail.com>
2020-02-09 22:28:59 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #self.count_sentences
+    string_array = []
+    string_array = self.split(". " || "! " || "? ")
+    string_array.count
   end
+
+  
 end
